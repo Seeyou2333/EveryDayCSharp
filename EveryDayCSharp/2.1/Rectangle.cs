7bd95ded9f3def5f99d78cd17f2fb01ae4e1b167
Seeyou2333 <1311141324@qq.com>
2020-11-17 14:38:29 +0800

    添加项目文件。

---
+++ EveryDayCSharp/2.1/Rectangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EveryDayCSharp
+{
+    //自定义类型
+    public class Rectangle
+    {
+        #region 数据成员
+        //const表示常量不可修改
+        public const double @const = 80;
+        public string @using { get; set; }
+        //类型是值的蓝本 int默认值为0
+        public int num { get; set; }
+
+        public double Heght { get; set; }
+        
+        public double Width { get; set; }
+        #endregion
+        //无参构造函数 无返回类型
+        public Rectangle()
+        {
+            //@using = "@可将保留关键字成为标识符";
+
+            //char c = '\n';//转义字符
+            //bool b = true;//布尔类型
+
+            //Console.WriteLine(@const);
+            //Console.WriteLine(num);
+            //Console.WriteLine(@using);
+            Console.WriteLine("构建矩形");
+        }
+        public Rectangle(double Heght, double Width)
+        {
+            //@using = "@可将保留关键字成为标识符";
+
+            //char c = '\n';//转义字符
+            //bool b = true;//布尔类型
+
+            //Console.WriteLine(@const);
+            //Console.WriteLine(num);
+            //Console.WriteLine(@using);
+            this.Width = Width;
+            this.Heght = Heght;
+            Console.WriteLine("构建矩形");
+        }
+
+        #region 函数成员
+        //public 修饰符 公开的 保护级别 
+        public double GetArea()
+        {
+            return Heght * Width;
+        }
+
+        public void WirteString()
+        {
+            Console.WriteLine("长:" + Heght + " 宽:" + Width);
+        }
+        #endregion
+
+
+    }
+}
---
+++ EveryDayCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EveryDayCSharp
+{
+    class Program
+    {
+        //static 静态成员  静态类（static class）中只能包含静态成员且不可被实例化
+        static void Main(string[] args)
+        {
+            //new进行实例化对象
+            var rectangle = new Rectangle() { Heght = 20.1,Width=33.56 };
+            //rectangle = new Rectangle(113, 222);
+            rectangle.WirteString();
+            Console.WriteLine("面积:"+rectangle.GetArea());
+            Console.ReadKey();
+        }
+
+        
+
+    }
+}
